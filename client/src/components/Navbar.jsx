655f4153572c5aef22346e283cf4f7f146223e53
Jason Moritz <moritz.jason@gmail.com>
2021-09-17 23:24:33 -0500

    added rough styling to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <div className="text-3xl p-10 text-right space-x-3">
-                <Link to="/">Home</Link>
-                <Link to="/players">Players</Link>
-                <Link to="/courses">Courses</Link>
-                <Link to="/addplayer">Add Player</Link>
-                <Link to="/addcourse">Add Course</Link>
+        <div className="flex items-center content-end">
+            <Link to="/">Home</Link>
+            <Link to="/players">Players</Link>
+            <Link to="/courses">Courses</Link>
+            <Link to="/addplayer">Add Player</Link>
+            <Link to="/addcourse">Add Course</Link>
+            <button class="fill-current hover: text-indigo-200 focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </button>
+                
         </div>
     )
 }
