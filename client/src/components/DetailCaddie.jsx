0056b47915586e00c9727f7b231bc6939e5db173
Jason Moritz <moritz.jason@gmail.com>
2021-09-25 09:15:35 -0500

    added conditional rendering to detailcaddie pg

--- client/src/components/DetailCaddie.jsx
+++ client/src/components/DetailCaddie.jsx
@@ -33,12 +33,117 @@ export default function PlayerDropDown() {
                 <DropDownMenu setID={setPlayerID} group="players" />
                 <label className="mt-4">Yardage Out</label>
                 <FormCaddie handleSubmit={handleSubmit} yardage={yardage} setYardage={setYardage} />
-                {club !== "" ? 
+                {club === "lw" ? 
                     <div className="text-2xl text-center mt-5">
-                        <h1>{`Don't be a hero. You need more club. Hit your ${club}. Believe me, you're not that guy, pal.`}</h1>
+                        <h1>Hit your lob wedge and pray you don't chunk it.</h1>
                     </div>
                     :
                     null    
+                }
+                {club === "sw" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>You will likely blade this but hit your sand wedge.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "aw" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your gap wedge and try not to pull it too far right or left.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "pw" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Let's try hitting your pitching wedge.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "nineir" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Trust me. You won't get there with how you hit wedges. Hit your 9 iron.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "eightir" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>You will likely try to swing too hard, but hit your 8 iron.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "sevenir" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>As long as you don't slice this, your 7 iron is the club to use.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "sixir" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your 6 iron. This will likely be chunked, but you need the practice.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "fiveir" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your 5 iron and swing extra hard because you will chunk this for sure.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "fourir" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your 4 iron and see what happens.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "threeir" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your 3 iron or hybrid.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "twoir" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your 2 iron or hybrid.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "hybrid" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your hybrid since this is likely the only club you can get the ball up in the air with.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "fivewd" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your 5 wood if you got one.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "threewd" ? 
+                    <div className="text-2xl text-center mt-5">
+                        <h1>Hit your 3 wood if you got one.</h1>
+                    </div>
+                    :
+                    null    
+                }
+                {club === "driver" ?
+                    <div className="text-2xl text-center mt-5">
+                            <h1>You really should be teeing off with your hybrid, but let's try and smash driver. Don't forget to swing extra hard.</h1>
+                        </div>
+                        :
+                        null    
                 }
             </div>
         </div>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <div>
-            <div className="text-center px-6 pt-10 text-lg text-gray-100 md:text-2xl">
+            <div className="text-center px-6 pt-10 text-xl text-gray-100 md:text-4xl">
             Hello. I'm the pocket caddie for you and your golfing buddies! Start by adding some players and courses and let's get hacking.. I mean playing better golf!
             </div>
             <div className="flex mt-10 justify-center">
-                <div className="w-1/2 flex-col text-green-lt text-xl font-semibold max-w-2xl md:text-xl">
+                <div className="w-1/2 flex-col text-green-lt text-xl font-semibold max-w-2xl md:w-1/5 md:text-3xl">
                     <Link to="/caddie">
                         <button className=" w-full border-4 rounded-2xl p-5 mb-5 border-gray-md  hover:bg-gray-md transform hover:scale-105 transition duration-400">Caddie</button>
                     </Link>
