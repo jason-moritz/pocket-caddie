4267c6205328ac3d78e5babd7bf2208020ede1f4
Jason Moritz <moritz.jason@gmail.com>
2021-09-25 22:01:11 -0500

    Add player mobile and lg screen styling complete.

--- client/src/forms/FormPlayer.jsx
+++ client/src/forms/FormPlayer.jsx
@@ -25,7 +25,7 @@ export default function PlayerForm(props) {
     return (
         <form className=" text-gray-100 w-screen mt-5" onSubmit={playerName !== "" ? handleSubmit : handleToggle}>
             <div className="flex justify-center">
-                <div className="w-3/4 max-w-lg text-xl flex flex-col items-center">
+                <div className="w-3/4 max-w-lg text-xl flex flex-col items-center sm:text-2xl">
                     <AddImage setImage={setPlayerImage} image={image} title="Selfie" />
                     <div className="mt-16">
                         <label>Player Name</label>
@@ -33,7 +33,7 @@ export default function PlayerForm(props) {
                         {toggle > 0 ? <p className="text-sm text-red-500">Please enter a player name</p> : null} 
 
                         <label>Handicap (0-54)</label>
-                        <a className="block text-sm text-center text-green-lt" rel="noreferrer" target="_blank" href="https://www.usga.org/course-handicap-calculator.html">Click here to learn more about golf handicaps</a>
+                        <a className="block text-sm  text-green-lt sm:text-lg" rel="noreferrer" target="_blank" href="https://www.usga.org/course-handicap-calculator.html">Click here to learn more about golf handicaps</a>
                         <input className="input" type="number" min="0" max="100" value={handicap} onChange={e => setHandicap(e.target.value)} />
 
 
