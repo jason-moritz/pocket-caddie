42faed44438a89c783969c445e1aeb3e93c7d0c5
Jason Moritz <moritz.jason@gmail.com>
2021-09-27 09:46:21 -0500

    Fixed slight mis-centering of pictures on players pg.

--- client/src/components/display/DisplayPlayers.jsx
+++ client/src/components/display/DisplayPlayers.jsx
@@ -23,28 +23,30 @@ export default function Players() {
                 <h1 className="title">
                     Players
                 </h1>
-                <div className="flex flex-col place-items-center pb-6 w-3/4 sm:flex-row sm:flex-wrap sm:justify-around sm:items-center">
-                    {players.map((player) => (
-                        <div className="mt-5 sm:w-1/5">
-                            <Link class="pt-2" to={`/players/${player.id}`}>
-                                <div className="flex flex-col place-items-center h-40 w-40 sm:h-48  sm:w-48 hover text-gray-300 transform transition-transform hover:scale-125 sm:hover:scale-110">
-                                    <div className="h-32  w-32 sm:h-40 sm:w-40 border-4 border-gray-100 rounded-full overflow-hidden">
-                                        <img 
-                                            className="h-full w-full object-cover object-center" 
-                                            src={player.fields?.playerImage} 
-                                            key={player.id} 
-                                            alt="player-profile"
-                                        />
+                <div className="flex w-full justify-center">
+                    <div className="flex flex-col place-items-center w-3/4 sm:flex-row sm:flex-wrap sm:justify-around sm:items-center">
+                        {players.map((player) => (
+                            <div className="my-5 mx-4 flex justify-center sm:w-1/5">
+                                <Link class="pt-2" to={`/players/${player.id}`}>
+                                    <div className="flex flex-col place-items-center h-40 w-40 sm:h-48 sm:w-48 hover text-gray-300 transform transition-transform hover:scale-125 sm:hover:scale-110">
+                                        <div className="h-32 w-32 sm:h-40 sm:w-40 border-4 border-gray-100 rounded-full overflow-hidden">
+                                            <img 
+                                                className="h-full w-full object-cover object-center" 
+                                                src={player.fields?.playerImage} 
+                                                key={player.id} 
+                                                alt="player-profile"
+                                            />
+                                        </div>
+                                        <div>
+                                            <h3 className="text-2xl sm:text-3xl">
+                                                {player.fields?.playerName}
+                                            </h3>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <h3 className="text-2xl sm:text-3xl">
-                                            {player.fields?.playerName}
-                                        </h3>
-                                    </div>
-                                </div>
-                            </Link>
-                        </div>
-                    ))}
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
