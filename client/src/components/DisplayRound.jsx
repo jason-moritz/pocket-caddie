120abd6d8c60146c1511f887217d1eb85011fbb6
Jason Moritz <moritz.jason@gmail.com>
2021-09-27 04:55:10 -0500

    Add round mobile and lg screen styling complete.

--- client/src/components/DisplayRound.jsx
+++ client/src/components/DisplayRound.jsx
@@ -1,8 +1,10 @@
 import AddRound from "./AddRound";
+import ButtonBack from "../buttons/ButtonBack";
 
 export default function PlayRound() {
     return (
         <div className="text-gray-100">
+            <ButtonBack />
             <AddRound />
         </div>
     )
--- client/src/components/DropDownMenu.jsx
+++ client/src/components/DropDownMenu.jsx
@@ -26,21 +26,52 @@ export default function DropDownMenuCaddie(props) {
     return (
         <div className="flex flex-col w-screen text-center items-center">
             {group === "players" ? 
-                <label className="text-xl sm:text-3xl mt-5">Select Player</label>
+                <label className="text-2xl sm:text-4xl mt-5">
+                    Select Player
+                </label>
             :
-                <label className="text-xl sm:text-3xl mt-5">Select Course</label>
+                <label className="text-2xl sm:text-4xl mt-5">
+                    Select Course
+                </label>
             }
-            <select className="relative inline-block text-lg sm:text-2xl bg-gray-dk border-none" onChange={twoCalls}>
+            <select 
+                className="relative inline-block text-xl sm:text-3xl bg-gray-dk border-none" 
+                onChange={twoCalls}
+            >
                 {group === "players" ?
-                    <option key="default" value="" className="absolute  bg-gray-dk  text-gray-100 shadow-none">Players</option>
+                    <option 
+                        key="default" 
+                        value="" 
+                        className="absolute  bg-gray-dk  text-gray-100 shadow-none"
+                    >
+                        Players
+                    </option>
                 :
-                    <option key="default" value="" className="absolute  bg-gray-dk  text-gray-100 shadow-none">Courses</option>
+                    <option 
+                        key="default" 
+                        value="" 
+                        className="absolute  bg-gray-dk  text-gray-100 shadow-none"
+                    >
+                        Courses
+                    </option>
                 }
                 {details.map((detail) => (
                     group === "players" ? 
-                        <option key={detail.id} value={detail.id} className=" bg-gray-dk  text-gray-100 outline-none focus:outline-none">{detail.fields?.playerName}</option>
+                        <option 
+                            key={detail.id} 
+                            value={detail.id} 
+                            className=" bg-gray-dk  text-gray-100 outline-none focus:outline-none"
+                        >
+                            {detail.fields?.playerName}
+                        </option>
                     :
-                        <option key={detail.id} value={detail.courseName} className="bg-gray-dk text-gray-100 outline-none focus:outline-none ">{detail.fields?.courseName}</option>
+                        <option 
+                            key={detail.id} 
+                            value={detail.courseName} 
+                            className="bg-gray-dk text-gray-100 outline-none focus:outline-none"
+                        >
+                            {detail.fields?.courseName}
+                        </option>
                 ))}
             </select>
         </div>
