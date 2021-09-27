0d5efdccec39ebb8c0891fc09b805326dc9ef1c7
Jason Moritz <moritz.jason@gmail.com>
2021-09-27 05:18:55 -0500

    Added info button and toggle on mainpage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
+
 export default function Home() {
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggle = (e) => {
+        e.preventDefault();
+
+        setToggle((prevToggle) => !prevToggle);
+    };
+
     return (
         <div className="flex flex-col items-center w-screen">
-            <div className="text-center w-full px-6 pt-10 text-xl text-gray-100 sm:text-4xl sm:w-full sm:px-40 sm:pb-10">
-            Hello. I'm the pocket caddie for you and your golfing buddies! Start by adding some players and courses and let's get hacking.. I mean playing better golf!
+            <div className="flex flex-col items-center text-center w-full px-8 pt-10 text-xl text-gray-100 sm:text-4xl sm:w-full sm:px-40 sm:pb-10">
+                Hello. I'm the pocket caddie for you and your golfing buddies! Start by adding some players and courses and let's get hacking.. I mean playing better golf!
+                <button className="h-8 w-8 block text-green-lt mt-4 sm:hidden" onClick={handleToggle}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </button>
             </div>
+            {toggle === true ? 
+                <div className="text-gray-100 text-base text-green-lt text-center w-full px-8 sm:hidden mt-4">
+                    Note - You need to add player yardages before using the caddie function. These can be added by selecting a player and selecting edit yardages.
+                </div>
+            :
+                null
+            }
             <div className="mt-10 w-full flex justify-center">
                 <div className="w-full flex flex-col items-center  text-green-lt text-xl font-semibold sm:text-3xl sm:flex-row">
                     <div className="flex flex-col w-3/4 sm:w-1/2 sm:mx-6">
@@ -26,6 +48,9 @@ export default function Home() {
                         </Link>
                     </div>
                 </div>
+            </div>
+            <div className="text-gray-100 text-base sm:text-2xl mt-16 md-down:hidden">
+                Note - You need to add player yardages before using the caddie function. These can be added by selecting a player and selecting edit yardages.
             </div>
         </div>
     )
--- client/src/forms/FormYardages.jsx
+++ client/src/forms/FormYardages.jsx
@@ -1,5 +1,42 @@
 export default function FormYardages(props) {
-    const { toggle, setToggle, lw, setLw, sw, setSw, aw, setAw, pw, setPw, nineir, setNineir, eightir, setEightir, sevenir, setSevenir, sixir, setSixir, fiveir, setFiveir, fourir, setFourir, threeir, setThreeir, twoir, setTwoir, fivewd, setFivewd, threewd, setThreewd, hybrid, setHybrid, driver, setDriver, handleSubmit, title } = props;
+    const { 
+            toggle, 
+            setToggle, 
+            lw, 
+            setLw, 
+            sw, 
+            setSw, 
+            aw, 
+            setAw, 
+            pw, 
+            setPw, 
+            nineir, 
+            setNineir, 
+            eightir, 
+            setEightir, 
+            sevenir, 
+            setSevenir, 
+            sixir, 
+            setSixir, 
+            fiveir, 
+            setFiveir, 
+            fourir, 
+            setFourir, 
+            threeir, 
+            setThreeir, 
+            twoir, 
+            setTwoir, 
+            fivewd, 
+            setFivewd, 
+            threewd, 
+            setThreewd, 
+            hybrid, 
+            setHybrid, 
+            driver, 
+            setDriver, 
+            handleSubmit, 
+            title 
+        } = props;
     
     const handleToggle = (e) => {
         e.preventDefault();
@@ -9,78 +46,196 @@ export default function FormYardages(props) {
 
     return (
         <form 
-        className="text-gray-100 w-screen mt-5 flex justify-center" 
-        onSubmit={lw && sw && aw && pw && nineir && eightir && sevenir && sixir && fiveir && fourir && threeir && twoir && hybrid && fivewd && threewd && driver ? handleSubmit : handleToggle}>
+            className="text-gray-100 w-screen mt-5 flex justify-center" 
+            onSubmit={lw && sw && aw && pw && nineir && eightir && sevenir && sixir && fiveir && fourir && threeir && twoir && hybrid && fivewd && threewd && driver ? handleSubmit : handleToggle}>
 
             <div className="flex text-xl w-full max-w-lg justify-center text-center">
                 <div className="md:flex md:flex-col md:w-full md:items-center">     
-                    {toggle > 0 ? <div className="mx-4 text-center text-lg mb-6 text-red-600">Please enter an average yardage for each club. If you do not have that club, please enter 0.</div> : null}               
+                    {toggle > 0 ? 
+                        <div className="mx-4 text-center text-lg mb-6 text-red-600">
+                            Please enter an average yardage for each club. If you do not have that club, please enter 0.
+                        </div> 
+                    : 
+                        null
+                    }               
                     <div className="md:flex md:flex-row">
                         <div>
                             <label>Lob Wedge</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={lw} onChange={e => setLw(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={lw} 
+                                onChange={e => setLw(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>Sand Wedge</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={sw} onChange={e => setSw(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={sw} 
+                                onChange={e => setSw(e.target.value)}
+                            />
                         </div>
                         <div>
                             <label>Gap Wedge</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={aw} onChange={e => setAw(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={aw} 
+                                onChange={e => setAw(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>Pitching Wedge</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={pw} onChange={e => setPw(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={pw} 
+                                onChange={e => setPw(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>9 Iron</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={nineir} onChange={e => setNineir(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={nineir} 
+                                onChange={e => setNineir(e.target.value)}  
+                            />
                         </div>
                         <div>
                             <label>8 Iron</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={eightir} onChange={e => setEightir(e.target.value)}     />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={eightir} 
+                                onChange={e => setEightir(e.target.value)}
+                            />
                         </div>
                         <div>
                             <label>7 Iron</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={sevenir} onChange={e => setSevenir(e.target.value)}     />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={sevenir} 
+                                onChange={e => setSevenir(e.target.value)}
+                            />
                         </div>
                         <div>
                             <label>6 Iron</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={sixir} onChange={e => setSixir(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={sixir} 
+                                onChange={e => setSixir(e.target.value)} 
+                            />
                         </div>
                     </div>
                     <div className="md:flex md:flex-row">
                         <div>
                             <label>5 Iron</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={fiveir} onChange={e => setFiveir(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={fiveir} 
+                                onChange={e => setFiveir(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>4 Iron</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={fourir} onChange={e => setFourir(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={fourir} 
+                                onChange={e => setFourir(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>3 Iron</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={threeir} onChange={e => setThreeir(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={threeir} 
+                                onChange={e => setThreeir(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>2 Iron</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={twoir} onChange={e => setTwoir(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={twoir} 
+                                onChange={e => setTwoir(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>Hybrid</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={hybrid} onChange={e => setHybrid(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={hybrid} 
+                                onChange={e => setHybrid(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>5 Wood</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={fivewd} onChange={e => setFivewd(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={fivewd} 
+                                onChange={e => setFivewd(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>3 Wood</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={threewd} onChange={e => setThreewd(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={threewd} 
+                                onChange={e => setThreewd(e.target.value)} 
+                            />
                         </div>
                         <div>
                             <label>Driver</label>
-                            <input className="input text-center md:w-36" type="number" min="0" max="500" value={driver} onChange={e => setDriver(e.target.value)} />
+                            <input 
+                                className="input text-center md:w-36" 
+                                type="number" 
+                                min="0" 
+                                max="500" 
+                                value={driver} 
+                                onChange={e => setDriver(e.target.value)} 
+                            />
                         </div>
                     </div>
                     <div className="flex justify-center">
